ea03f75ae6fb41d13e95004edd318447845d42de
FranklineMisango <franklinemisango@gmail.com>
2025-07-28 13:05:27 +0200

    yf + yt analytics

--- obj/Debug/net8.0/QuantResearchAgent.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuantResearchAgent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantResearchAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739771360e1dd11e80bcfa4cb54bd4399a883085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e878645e72ea963393bd0968429a7c372b8d594")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantResearchAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantResearchAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
